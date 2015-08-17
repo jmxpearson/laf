c82a9fe028d7459d2e51bee555fe7a5e298b4de4
John Pearson <jmxpearson@gmail.com>
2015-08-17 16:18:30 -0400

    nGP sampling now accepts state number
    - code now correctly decouples number of underlying states from number
      of observations
    - number of states in state space model is 3x this as per Zhu and Dunson
    - tests passing

--- NGPtools.jl
+++ NGPtools.jl
@@ -6,11 +6,15 @@ import SStools.simulate
 export assemble_matrices, generate, sample
 
 """
-Observation matrix for state space model. In this case, simply picks out
-the first entry of the state space vector, which is U.
+Observation matrix for state space model. In this case, we assume that the
+Np components of the observation vector each track the state of the same 
+index (more specifically, its U component). All other terms are ignored.
+Np is the dimension of the observation vector.
+Ns is the number of underlying states.
 """
-function Z(Np)
-    Zmat = sparse(1:Np, 1:Np, 1, Np, 3Np)
+function Z(Np, Ns)
+    maxind = min(3Ns, Np)
+    Zmat = sparse(1:maxind, 1:maxind, 1, Np, 3Ns)
     return Zmat
 end
 
@@ -18,12 +22,13 @@ end
 """
 State transition matrix. Defined in (5) of Zhu and Dunson. Order of entries
 is all state variables, followed by all derivatives, then all latents.
+Ns is the dimension of the underlying state space.
 """
-function G(Np, δ, σU, σA; approx=false)
-    gmat = eye(3 * Np)
-    gmat += diagm(δ * ones(2Np), Np)
+function G(Ns, δ, σU, σA; approx=false)
+    gmat = eye(3 * Ns)
+    gmat += diagm(δ * ones(2Ns), Ns)
     if approx
-        gmat += diagm(δ^2/2 * ones(Np), 2Np)
+        gmat += diagm(δ^2/2 * ones(Ns), 2Ns)
     end
 
     return sparse(gmat)
@@ -34,21 +39,21 @@ end
 Covariance of state disturbances. Defined in (5) of Zhu and Dunson. Order of
 entries is all state variables, followed by all derivatives, then all latents.
 """
-function W(Np, δ, varU, varA; approx=false)
+function W(Ns, δ, varU, varA; approx=false)
     if approx
-        wmat = δ * spdiagm(repeat([varU, varA], inner=[Np]))
+        wmat = δ * spdiagm(repeat([varU, varA], inner=[Ns]))
     else
-        wmat = zeros(Float64, 3Np, 3Np)
-        for idx in 1:Np
+        wmat = zeros(Float64, 3Ns, 3Ns)
+        for idx in 1:Ns
             wmat[idx, idx] = (δ^3 / 3)varU + (δ^5 / 20)varA
-            wmat[idx, Np + idx] = (δ^2 / 2)varU + (δ^4 / 8)varA
-            wmat[idx, 2Np + idx] = (δ^3 / 6)varA
-            wmat[Np + idx, idx] = wmat[idx, Np + idx]
-            wmat[Np + idx, Np + idx] = δ * varU + (δ^3 / 3)varA
-            wmat[Np + idx, 2Np + idx] = (δ^2 / 2)varA
-            wmat[2Np + idx, idx] = wmat[idx, 2Np + idx]
-            wmat[2Np + idx, Np + idx] = wmat[Np + idx, 2Np + idx]
-            wmat[2Np + idx, 2Np + idx] = δ * varA
+            wmat[idx, Ns + idx] = (δ^2 / 2)varU + (δ^4 / 8)varA
+            wmat[idx, 2Ns + idx] = (δ^3 / 6)varA
+            wmat[Ns + idx, idx] = wmat[idx, Ns + idx]
+            wmat[Ns + idx, Ns + idx] = δ * varU + (δ^3 / 3)varA
+            wmat[Ns + idx, 2Ns + idx] = (δ^2 / 2)varA
+            wmat[2Ns + idx, idx] = wmat[idx, 2Ns + idx]
+            wmat[2Ns + idx, Ns + idx] = wmat[Ns + idx, 2Ns + idx]
+            wmat[2Ns + idx, 2Ns + idx] = δ * varA
         end
     end
 
@@ -61,25 +66,26 @@ Take nested GP parameters and return matrices suitable for feeding into
 state space model.
 
 Np is the dimension of the observation vector
+Ns is the number of underlying states
 δ is a vector of differences between observation times. If observations are 
 uniformly spaced, δ can be a scalar or a length-1 vector.
 """
-function assemble_matrices(Np, δ, σϵ, σU, σA, σμ, σα; approx=false)
+function assemble_matrices(Np, Ns, δ, σϵ, σU, σA, σμ, σα; approx=false)
     Nt = length(δ)  # number of time points
 
     # calculate dimensions
     # Note: for an observation vector of size Np, we introduce 3 latent
     # variables per observation and 2 or 3 noise variables (depending on 
     # approximation)
-    Nm = 3 * Np
+    Nm = 3 * Ns
     if approx
-        Nr = 2 * Np
+        Nr = 2 * Ns
     else
-        Nr = 3 * Np
+        Nr = 3 * Ns
     end
 
     # preallocate arrays:
-    H = σϵ^2 * eye(Np)
+    H = σϵ^2 * eye(Ns)
     T = Array(Float64, Nm, Nm, Nt)
     Q = Array(Float64, Nr, Nr, Nt)
 
@@ -90,8 +96,8 @@ function assemble_matrices(Np, δ, σϵ, σU, σA, σμ, σα; approx=false)
     end
 
     for t in 1:Nt
-        T[:, :, t] = G(Np, δ[t], σU, σA, approx=approx)
-        Q[:, :, t] = W(Np, δ[t], σU^2, σA^2, approx=approx)
+        T[:, :, t] = G(Ns, δ[t], σU, σA, approx=approx)
+        Q[:, :, t] = W(Ns, δ[t], σU^2, σA^2, approx=approx)
     end
 
     if Nt == 1
@@ -103,9 +109,9 @@ function assemble_matrices(Np, δ, σϵ, σU, σA, σμ, σα; approx=false)
     end
 
     a0 = zeros(Nm)
-    P0 = diagm(repeat([σμ, σμ, σα], inner=[Np]))
+    P0 = diagm(repeat([σμ, σμ, σα], inner=[Ns]))
 
-    return Z(Np), H, T, R, Q, a0, P0
+    return Z(Np, Ns), H, T, R, Q, a0, P0
 end
 
 
@@ -114,11 +120,12 @@ Generate nGP data according to the state space model. Uses matrices
 constructed in assemble_matrices.
 
 Np is the dimension of the observation vector
+Ns is the number of underlying states
 δ is a spacing between observations
 """
-function generate(Np, Nt, δ, σϵ, σU, σA, σμ, σα; approx=false)
+function generate(Np, Ns, Nt, δ, σϵ, σU, σA, σμ, σα; approx=false)
 
-    Z, H, T, R, Q, a0, P0 = assemble_matrices(Np, δ, σϵ, σU, σA, σμ, σα, approx=approx)
+    Z, H, T, R, Q, a0, P0 = assemble_matrices(Np, Ns, δ, σϵ, σU, σA, σμ, σα, approx=approx)
 
     Nm = size(Z, 2)
 
@@ -147,7 +154,6 @@ end
 
 """
 Sample Ns times from the nGP posterior, given observations y.
-Np is the dimension of the observation vector
 """
 function _sample(y, Ns, Nt, a0, P0, Z, H, T, R, Q)
     Nm = size(Z, 2)
@@ -166,9 +172,9 @@ end
 Version where spacing is uniform, so number of time points must be specified 
 explicitly.
 """
-function sample(y, Ns, Np, Nt, δ::Float64, σϵ, σU, σA, σμ, σα; approx=false)
-    Z, H, T, R, Q, a0, P0 = assemble_matrices(Np, δ, σϵ, σU, σA, σμ, σα, approx=approx)
-    _sample(y, Ns, Nt, a0, P0, Z, H, T, R, Q)
+function sample(y, Nsamples, Np, Nt, Nstates, δ::Float64, σϵ, σU, σA, σμ, σα; approx=false)
+    Z, H, T, R, Q, a0, P0 = assemble_matrices(Np, Nstates, δ, σϵ, σU, σA, σμ, σα, approx=approx)
+    _sample(y, Nsamples, Nt, a0, P0, Z, H, T, R, Q)
 end
 
 
@@ -176,9 +182,9 @@ end
 Version where spacing is non-uniform, so δ is a vector and number of time
 points follows from that.
 """
-function sample(y, Ns, Np, δ::Vector{Any}, σϵ, σU, σA, σμ, σα; approx=false)
+function sample(y, Nsamples, Np, Nstates, δ::Vector{Any}, σϵ, σU, σA, σμ, σα; approx=false)
     Nt = length(δ)
-    Z, H, T, R, Q, a0, P0 = assemble_matrices(Np, δ, σϵ, σU, σA, σμ, σα, approx=approx)
+    Z, H, T, R, Q, a0, P0 = assemble_matrices(Np, Nstates, δ, σϵ, σU, σA, σμ, σα, approx=approx)
     _sample(y, Ns, Nt, a0, P0, Z, H, T, R, Q)
 end
 
@@ -186,9 +192,10 @@ end
 """
 Version where observation model (Z and H) is specified externally.
 """
-function sample(y, Ns, Np, Z::Matrix{Any}, H::Matrix{Any}, δ::Float64, 
-    σϵ, σU, σA, σμ, σα; approx=false)
-    _, _, T, R, Q, a0, P0 = assemble_matrices(Np, δ, σϵ, σU, σA, σμ, σα, approx=approx)
+function sample(y, Nsamples, Np, Z::Matrix{Any}, H::Matrix{Any}, δ::Float64, 
+    σU, σA, σμ, σα; approx=false)
+    Nstates = size(Z, 2)
+    _, _, T, R, Q, a0, P0 = assemble_matrices(Np, Nstates, δ, 0, σU, σA, σμ, σα, approx=approx)
     _sample(y, Ns, Nt, a0, P0, Z, H, T, R, Q)
 end
 
